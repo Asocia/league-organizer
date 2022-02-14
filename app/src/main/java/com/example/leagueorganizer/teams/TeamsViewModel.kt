81d6145bc57a1979a5b2cb08dfa352d1df5c43d6
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-02-14 12:21:37 +0300

    Create viewmodel for retrieving team names

---
+++ app/src/main/java/com/example/leagueorganizer/teams/TeamsViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.leagueorganizer.teams
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.leagueorganizer.network.TeamsApi
+import kotlinx.coroutines.launch
+
+class TeamsViewModel: ViewModel() {
+    private val _status = MutableLiveData<String>()
+
+    val status: LiveData<String> = _status
+    
+    fun getTeamNames(quantity: String) {
+        viewModelScope.launch {
+
+            try {
+                val listResult = TeamsApi.RETROFIT_SERVICE.getTeamNames("surname", quantity)
+                _status.value = listResult.toString()
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+        }
+    }
+}
