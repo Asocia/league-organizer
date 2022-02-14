cfbf90b47377bb74944992931e68a771bd23d24c
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-02-14 12:11:56 +0300

    Implement basic functionality for main activity

--- app/src/main/java/com/example/leagueorganizer/MainActivity.kt
+++ app/src/main/java/com/example/leagueorganizer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.leagueorganizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<NumberPicker>(R.id.npNumOfTeams).apply {
+            maxValue = 16
+            minValue = 3
+            wrapSelectorWheel = false
+        }
+
+        findViewById<Button>(R.id.btnDrawFixture).apply {
+            setOnClickListener { drawFixture()
+            }
+        }
+    }
+
+    private fun drawFixture() {
+        TODO("Not yet implemented")
     }
 }
